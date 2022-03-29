1d4b3a7551802fbe2f5544859d2a6aa5ec8df88f
Luiz Carlos Silva de Araujo <31112041+LuizCarlosS@users.noreply.github.com>
2022-03-28 20:52:38 -0400

    initial commit - tensor tests

---
+++ tutorial_workspace/tensors.py
@@ -0,0 +1,75 @@
+# code from https://pytorch.org/tutorials/beginner/blitz/tensor_tutorial.html
+
+import torch
+import numpy as np
+
+data = [[1, 2], [3, 4]]
+x_data = torch.tensor(data)
+
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+
+x_ones = torch.ones_like(x_data)  # retains the properties of x_data
+print(f"Ones Tensor: \n {x_ones} \n")
+
+# overrides the datatype of x_data
+x_rand = torch.rand_like(x_data, dtype=torch.float)
+print(f"Random Tensor: \n {x_rand} \n")
+
+shape = (2, 3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+
+tensor = torch.rand(3, 4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
+
+# We move our tensor to the GPU if available
+if torch.cuda.is_available():
+    tensor = tensor.to('cuda')
+    print(f"Device tensor is stored on: {tensor.device}")
+
+tensor = torch.ones(4, 4)
+tensor[:, 1] = 0
+print(tensor)
+
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1.shape)
+
+t1 = torch.stack([tensor, tensor, tensor], dim=1)
+print(t1.shape)
+
+# This computes the element-wise product
+print(f"tensor.mul(tensor) \n {tensor.mul(tensor)} \n")
+# Alternative syntax:
+print(f"tensor * tensor \n {tensor * tensor}")
+
+print(f"tensor.matmul(tensor.T) \n {tensor.matmul(tensor.T)} \n")
+# Alternative syntax:
+print(f"tensor @ tensor.T \n {tensor @ tensor.T}")
+
+print(tensor, "\n")
+tensor.add_(5)
+print(tensor)
+
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+t.add_(1)
+print(f"t: {t}")
+print(f"n: {n}")
+
+n = np.ones(5)
+t = torch.from_numpy(n)
+np.add(n, 1, out=n)
+print(f"t: {t}")
+print(f"n: {n}")
